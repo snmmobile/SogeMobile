945987096e276dba574a8a7e3fb934137270ab48
Bernado Myrtil <bernadomyrtil@gmail.com>
2026-08-17 18:06:25 -0400

    Release admin-managed TMP display override

--- scripts/build-signed-release.ps1
+++ scripts/build-signed-release.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$KeystorePath = "$env:USERPROFILE\Documents\apps\signing\sogemobile-release.jks",
-    [string]$DestinationPath = "$env:USERPROFILE\Documents\apps\SogeMobile-v1.3.apk"
+    [string]$DestinationPath = "$env:USERPROFILE\Documents\apps\SogeMobile-v1.4.apk"
 )
 
 $ErrorActionPreference = 'Stop'
